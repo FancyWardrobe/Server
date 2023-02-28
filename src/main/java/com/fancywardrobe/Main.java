0fa5022b63e14a95de1f3eec9d08f6f2ac028845
DudkoAndrei <dudkoandrei@gmail.com>
2023-02-28 15:35:18 +0300

    feat: Add code sample and test sample

---
+++ src/main/java/com/fancywardrobe/Main.java
@@ -0,0 +1,9 @@
+package com.fancywardrobe;
+
+
+public class Main {
+
+  public static void main(String[] args) {
+    System.out.printf("Hello");
+  }
+}
---
+++ src/test/java/com/fancywardrobe/SampleTest.java
@@ -0,0 +1,13 @@
+package com.fancywardrobe;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+public class SampleTest {
+
+  @Test
+  public void test() {
+    assertEquals(4, 2 * 2);
+  }
+}
