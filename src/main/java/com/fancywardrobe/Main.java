64a2c31599baab6cc8fceba9da60a7429fcd78d6
Andrei <67242525+DudkoAndrei@users.noreply.github.com>
2023-03-10 22:38:34 +0300

    pr(#3): Setup CI workflows
    * feat: Add code sample and test sample
    
    * build: Add gradle wrapper
    
    * ci: Add build workflow
    
    * fix(ci): Change JDK version to 18
    
    * fix(ci): Change JDK version back to 19 and update sonarqube
    
    * fix: Make main method empty
    
    * refactor(test): Remove access modifiers from SampleTest
    
    * test(SampleTest): Break test
    
    * ci(build): Add test report upload
    
    * fix(SampleTest): Correct expected value

---
+++ src/main/java/com/fancywardrobe/Main.java
@@ -0,0 +1,9 @@
+package com.fancywardrobe;
+
+
+public class Main {
+
+  public static void main(String[] args) {
+    // Not implemented yet
+  }
+}
---
+++ src/test/java/com/fancywardrobe/SampleTest.java
@@ -0,0 +1,13 @@
+package com.fancywardrobe;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+class SampleTest {
+
+  @Test
+  void test() {
+    assertEquals(4, 2 * 2);
+  }
+}
